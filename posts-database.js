4731140de27fcb3b16cc9fae5206f931e61652a3
arahman230505-netizen <a.rahman230505@gmail.com>
2025-11-01 19:02:38 +0000

    Fix: PAGES WORKING

--- posts-database.js
+++ posts-database.js
@@ -8,13 +8,31 @@ function savePosts(posts) {
   localStorage.setItem("posts", JSON.stringify(posts));
 }
 
-function addPost(username, title, content) {
+function addPost(username, title, content, image=null) {
   const posts = getPosts();
-  posts.push({ username, title, content, date: new Date().toISOString() });
+  posts.push({ id: Date.now(), username, title, content, image, date: new Date().toISOString(), likes: [] });
   savePosts(posts);
 }
 
-function getUserPosts(username) {
+function deletePost(post) {
+  const posts = getPosts().filter(p => p.id !== post.id);
+  savePosts(posts);
+}
+
+function toggleLike(post, username) {
   const posts = getPosts();
-  return posts.filter(p => p.username === username);
+  const index = posts.findIndex(p => p.id === post.id);
+  if(index > -1){
+    const likes = posts[index].likes || [];
+    if(likes.includes(username)){
+      posts[index].likes = likes.filter(u=>u!==username);
+    } else {
+      posts[index].likes.push(username);
+    }
+    savePosts(posts);
+  }
+}
+
+function getUserPosts(username) {
+  return getPosts().filter(p => p.username === username);
 }
