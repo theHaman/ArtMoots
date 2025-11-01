023b30971e381c37bb6b93a45c65510383fc1b99
arahman230505-netizen <a.rahman230505@gmail.com>
2025-11-01 22:43:43 +0000

    going back

--- posts-database.js
+++ posts-database.js
@@ -1,50 +1,29 @@
 // posts-database.js
-
-// Base URL of your server
-const API_URL = "http://localhost:3000/posts";
+let posts = JSON.parse(localStorage.getItem("posts") || "[]");
 
 // Get all posts
-async function getPosts() {
-  try {
-    const res = await fetch(API_URL);
-    return await res.json();
-  } catch (err) {
-    console.error("Failed to fetch posts:", err);
-    return [];
-  }
+function getPosts() {
+  return posts;
 }
 
-// Add a new post
-async function addPost(post) {
-  try {
-    await fetch(API_URL, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(post),
-    });
-  } catch (err) {
-    console.error("Failed to add post:", err);
-  }
+// Add a post
+function addPost(post) {
+  posts.push(post);
+  localStorage.setItem("posts", JSON.stringify(posts));
 }
 
-// Edit a post (only editable by author)
-async function editPostDB(id, updatedPost) {
-  try {
-    await fetch(`${API_URL}/${id}`, {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(updatedPost),
-    });
-  } catch (err) {
-    console.error("Failed to edit post:", err);
+// Edit a post
+function editPostDB(index, newData) {
+  if(posts[index]){
+    posts[index] = {...posts[index], ...newData};
+    localStorage.setItem("posts", JSON.stringify(posts));
   }
 }
 
 // Delete a post
-async function deletePostDB(id) {
-  try {
-    await fetch(`${API_URL}/${id}`, { method: "DELETE" });
-  } catch (err) {
-    console.error("Failed to delete post:", err);
+function deletePostDB(index) {
+  if(posts[index]){
+    posts.splice(index,1);
+    localStorage.setItem("posts", JSON.stringify(posts));
   }
 }
